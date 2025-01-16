e196778719f5b57686d179d88485129dba1ce61d
maciej022 <maciejpieper56@gmail.com>
2025-01-16 11:21:54 +0100

    dzialajacy kod

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿Notatka PierwszaNotatka = new Notatka("Pierwsza", "zawartosc 1");
+Notatka DrugaNotatka = new Notatka("Druga", "zawartosc 2");
+PierwszaNotatka.Wyswietlanie();
+PierwszaNotatka.Diagnostyka();
+DrugaNotatka.Wyswietlanie();
+DrugaNotatka.Diagnostyka();
+class Notatka
+{
+    private static int Licznik = 0;
+    private int id;
+    protected string Tytul;
+    protected string Tresc;
+    public Notatka(string _tytul,string _tresc)
+    {
+        Tytul=_tytul;
+        Tresc = _tresc;
+        Licznik++;
+        id = Licznik;
+    }
+    public void Wyswietlanie()
+    {
+        Console.WriteLine(Tytul);
+        Console.WriteLine(Tresc);
+    }
+    public void Diagnostyka()
+    {
+        Console.Write(id.ToString());
+        Console.Write(";");
+        Console.Write(Tytul);
+        Console.Write(";");
+        Console.WriteLine(Tresc);
+    }
+}
